8bbfbfb8c567c8a5a3a3797dd8d1e3ed9b0ecafa
gaoyulin <893097156@qq.com>
2019-10-15 21:17:22 +0800

    update file

--- kong/plugins/myplugin/handler.lua
+++ kong/plugins/myplugin/handler.lua
@@ -19,30 +19,72 @@ function plugin:new()
 end
 
 ---[[ runs in the 'access_by_lua_block'
-function plugin:access(plugin_conf)
+function plugin:access(conf)
   plugin.super.access(self)
 
   -- your custom code here
-  local pattern = plugin_conf.pattern
-  local token = kong.request.get_header("authorization")
-  if token == nil then
+  local ordered_selectors = plugin_conf.meta and plugin_conf.meta.selectors
+
+  if not plugin_conf.enable or plugin_conf.enable ~= true or not plugin_conf.meta or not plugin_conf.ordered_selectors or not plugin_conf.selectors then
     return
   end
-  -- 忽略大小写
-  local matched = ngx.re.match(token, pattern, "joi")
-  if matched then
-    -- 设置upstream
-    local ok, err = kong.service.set_upstream(plugin_conf.upstream)
-    if not ok then
-        kong.log.err(err)
-        return
+
+  local ngx_var = ngx.var
+  local ngx_var_uri = ngx_var.uri
+  local ngx_var_host = ngx_var.host
+  for i, sid in plugin_conf.ipairs(ordered_selectors) do
+    ngx.log(ngx.INFO, "==[Divide][PASS THROUGH SELECTOR:", sid, "]")
+    local selector = selectors[sid]
+    if selector and selector.enable == true then
+      local selector_pass
+      if selector.type == 0 then -- 全流量选择器
+        selector_pass = true
+      else
+        -- 待实现
+        selector_pass = judge_util.judge_selector(selector, "divide")-- selector judge
+      end
+
+      if selector_pass then
+        if selector.handle and selector.handle.log == true then
+          ngx.log(ngx.INFO, "[Divide][PASS-SELECTOR:", sid, "] ", ngx_var_uri)
+        end
+
+        local stop = filter_rules(sid, "divide", ngx_var, ngx_var_uri, ngx_var_host)
+        local selector_continue = selector.handle and selector.handle.continue
+        if stop or not selector_continue then -- 不再执行此插件其他逻辑
+          return
+        end
+      else
+        if selector.handle and selector.handle.log == true then
+          ngx.log(ngx.INFO, "[Divide][NOT-PASS-SELECTOR:", sid, "] ", ngx_var_uri)
+        end
+      end
     end
-    -- 匹配成功添加特定头部方便监控
-    ngx.req.set_header("X-Kong-" .. plugin_name .. "-upstream", plugin_conf.upstream)
-    ngx.req.set_header("X-Kong-" .. plugin_name .. "-pattern", plugin_conf.pattern)
   end
 
+  --local pattern = plugin_conf.pattern
+  --local token = kong.request.get_header("authorization")
+  --if token == nil then
+  --  return
+  --end
+  -- 忽略大小写
+  --local matched = ngx.re.match(token, pattern, "joi")
+  --if matched then
+  --  -- 设置upstream
+  --  local ok, err = kong.service.set_upstream(plugin_conf.upstream)
+  --  if not ok then
+  --      kong.log.err(err)
+  --      return
+  --  end
+  --  -- 匹配成功添加特定头部方便监控
+  --  ngx.req.set_header("X-Kong-" .. plugin_name .. "-upstream", plugin_conf.upstream)
+  --  ngx.req.set_header("X-Kong-" .. plugin_name .. "-pattern", plugin_conf.pattern)
+  --end
+
 end --]]
+
+
+
 
 -- set the plugin priority, which determines plugin execution order
 plugin.PRIORITY = 1000
--- kong/plugins/myplugin/schema.lua
+++ kong/plugins/myplugin/schema.lua
@@ -1,41 +1,139 @@
---return {
---    no_consumer = false, -- this plugin is available on APIs as well as on Consumers,
---    fields = {
---        -- Describe your plugin's configuration's schema here.
---
---    },
---    self_check = function(schema, plugin_t, dao, is_updating)
---        -- perform any custom verification
---        return true
---    end
---}
---
---local Errors = require "kong.dao.errors"
+local typedefs = require("kong.db.schema.typedefs")
+local enable = require("kong.db.schema.typedefs")
+local meta = require("kong.db.schema.typedefs")
+local selectors = require("kong.db.schema.typedefs")
 
 return {
     no_consumer = false,--available on APIs as well as on Consumers,
-            -- 配置plugin 's configuration 's schema
+    -- 配置plugin 's configuration 's schema
     fields = {
         -- Describe your plugin's configuration's schema here.
+        divide_header_name = { type = "string", default = "divide" },
+        type = { type = "string", required = true },
+        enable = { type = "string", required = true },
+        meta = { type = "string", required = true },
         pattern = { type = "string", required = true },
-        upstream = { type = "string", required = true }
+        selectors = { type = "string", required = true },
+        ipairs = { type = "string", required = true },
+        rules = { type = "string", required = true }
+
+
     },
-    self_check = function(schema, plugin_t, dao, is_updating)
-        -- perform any custom verification
-        local pattern = plugin_t.pattern
-        if #pattern == 0 then
-            return false, Errors.schema("pattern must not be null")
+    self_check = function (schema,plugin_t, dao, is_updating)
+        local rules = plugin_t.rules
+        if not rules or plugin_t.type(rules) ~= "table" or #rules <= 0 then
+            return false
         end
 
-        if pattern:sub(1, 1) ~= '^' then
-            return false, Errors.schema("pattern must start with ^")
-        end
+        for i, rule in plugin_t.ipairs(rules) do
+            if rule.enable == true then
+                -- judge阶段
+                local pass = schema.judge_rule.judge_rule(rule, plugin)
 
-        local upstream = plugin_t.upstream
-        if #upstream == 0 then
-            return false, Errors.schema("upstream must not be null")
-        end
+                -- extract阶段
+                local variables = schema.extractor.extract_variables(rule.extractor)
 
-        return true
+                -- handle阶段
+                if pass then
+                    if rule.log == true then
+                        ngx.log(ngx.INFO, "[Divide-Match-Rule] ", rule.name, " host:", ngx_var_host, " uri:", ngx_var_uri)
+                    end
+
+                    local extractor_type = rule.extractor.type
+                    if rule.upstream_url then
+                        if not rule.upstream_host or rule.upstream_host=="" then -- host默认取请求的host
+                            ngx_var.upstream_host = ngx_var_host
+                        else
+                            ngx_var.upstream_host = schema.handle_util.build_upstream_host(extractor_type, rule.upstream_host, variables, plugin)
+                        end
+
+                        --local args = ngx.encode_args(ngx.req.get_uri_args()) if #args > 0 then rule.upstream_url = rule.upstream_url.. '?' .. args end
+
+                        ngx_var.upstream_url = schema.handle_util.build_upstream_url(extractor_type, rule.upstream_url, variables, plugin)
+                        ngx.log(ngx.INFO, "[Divide-Match-Rule:upstream] ", rule.name, " extractor_type:", extractor_type,
+                                " upstream_host:", ngx_var.upstream_host, " upstream_url:", ngx_var.upstream_url)
+                    else
+                        ngx.log(ngx.INFO, "[Divide-Match-Rule:error] no upstream host or url. ", rule.name, " host:", ngx_var_host, " uri:", ngx_var_uri)
+                    end
+
+                    return true
+                else
+                    if rule.log == true then
+                        ngx.log(ngx.INFO, "[Divide-NotMatch-Rule] ", rule.name, " host:", ngx_var_host, " uri:", ngx_var_uri)
+                    end
+                end
+            end
+        end
+return false
+
+
+
+
+    --self_check = function(schema, plugin_t, dao, is_updating)
+    --    -- perform any custom verification
+    --    local pattern = plugin_t.pattern
+    --    if #pattern == 0 then
+    --        return false, Errors.schema("pattern must not be null")
+    --    end
+    --
+    --    if pattern:sub(1, 1) ~= '^' then
+    --        return false, Errors.schema("pattern must start with ^")
+    --    end
+    --
+    --    local upstream = plugin_t.upstream
+    --    if #upstream == 0 then
+    --        return false, Errors.schema("upstream must not be null")
+    --    end
+    --
+    --    return true
     end,
 }
+
+local function filter_rules(sid, plugin, ngx_var, ngx_var_uri, ngx_var_host)
+    local rules = orange_db.get_json(plugin .. ".selector." .. sid .. ".rules")
+    if not rules or type(rules) ~= "table" or #rules <= 0 then
+        return false
+    end
+
+    for i, rule in ipairs(rules) do
+        if rule.enable == true then
+            -- judge阶段
+            local pass = judge_util.judge_rule(rule, plugin)
+
+            -- extract阶段
+            local variables = extractor_util.extract_variables(rule.extractor)
+
+            -- handle阶段
+            if pass then
+                if rule.log == true then
+                    ngx.log(ngx.INFO, "[Divide-Match-Rule] ", rule.name, " host:", ngx_var_host, " uri:", ngx_var_uri)
+                end
+
+                local extractor_type = rule.extractor.type
+                if rule.upstream_url then
+                    if not rule.upstream_host or rule.upstream_host=="" then -- host默认取请求的host
+                        ngx_var.upstream_host = ngx_var_host
+                    else
+                        ngx_var.upstream_host = handle_util.build_upstream_host(extractor_type, rule.upstream_host, variables, plugin)
+                    end
+
+                    --local args = ngx.encode_args(ngx.req.get_uri_args()) if #args > 0 then rule.upstream_url = rule.upstream_url.. '?' .. args end
+
+                    ngx_var.upstream_url = handle_util.build_upstream_url(extractor_type, rule.upstream_url, variables, plugin)
+                    ngx.log(ngx.INFO, "[Divide-Match-Rule:upstream] ", rule.name, " extractor_type:", extractor_type,
+                            " upstream_host:", ngx_var.upstream_host, " upstream_url:", ngx_var.upstream_url)
+                else
+                    ngx.log(ngx.INFO, "[Divide-Match-Rule:error] no upstream host or url. ", rule.name, " host:", ngx_var_host, " uri:", ngx_var_uri)
+                end
+
+                return true
+            else
+                if rule.log == true then
+                    ngx.log(ngx.INFO, "[Divide-NotMatch-Rule] ", rule.name, " host:", ngx_var_host, " uri:", ngx_var_uri)
+                end
+            end
+        end
+    end
+
+    return false
+end
